12093628b0cf2aafb573d16d75d15df031970e2c
soe1hom-arch <soe1hom@gmail.com>
2026-06-11 23:11:02 +0700

    Update filesystem.py

--- modules/filesystem.py
+++ modules/filesystem.py
@@ -1,6 +1,6 @@
 # ============================================================
-# Android Firmware Full Toolkit (AFFT) v2.0
-# By. soe1hom-arch / Wandi
+# Android Firmware Full Toolkit (AFFT) v2.0.1
+# Author. soe1hom-arch / Wandi
 # ============================================================
 
 import os
